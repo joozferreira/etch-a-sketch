555eb6fe8ab176ac7130c0413894b9daf65dcdaf
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-11-07 00:30:32 +0000

    Add event listener for click on game mode

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+// Change of game mode
+const buttons = Array.from(document.getElementsByClassName("mode"));
+buttons.forEach( (button) => {
+  button.addEventListener( "click", (e) => {
+    for (let i = 0; i < buttons.length; i++) {
+      if (buttons[i].getAttribute("id") === e.target.getAttribute("id")) {
+        buttons[i].classList.add("selected");
+      } else {
+        buttons[i].classList.remove("selected");
+      }
+    }
+  })
+});
+
+
 // Link between slider and paragraph field
 const sliderSize = document.getElementById("size");
 
-sliderSize.addEventListener('input', () => {
+sliderSize.addEventListener("input", () => {
   let selectedGrid = document.getElementById("selected-grid");
   selectedGrid.textContent = `${sliderSize.value} X ${sliderSize.value}`;
 });
