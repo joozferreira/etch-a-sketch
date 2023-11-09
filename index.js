4eeac3508ae50ea909192ac4d1940565ac267261
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-11-09 23:28:37 +0000

    Add global variable to store settings

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Global variable to define the settings of the game as an array
+const settings = ['classic', 16];
+
 // Change of game mode
 const buttons = Array.from(document.getElementsByClassName("mode"));
 buttons.forEach( (button) => {
@@ -5,6 +8,7 @@ buttons.forEach( (button) => {
     for (let i = 0; i < buttons.length; i++) {
       if (buttons[i].getAttribute("id") === e.target.getAttribute("id")) {
         buttons[i].classList.add("selected");
+        settings[0] = buttons[i].getAttribute("id");
       } else {
         buttons[i].classList.remove("selected");
       }
@@ -19,4 +23,5 @@ const sliderSize = document.getElementById("size");
 sliderSize.addEventListener("input", () => {
   let selectedGrid = document.getElementById("selected-grid");
   selectedGrid.textContent = `${sliderSize.value} X ${sliderSize.value}`;
+  settings[1] = sliderSize.value;
 });
