5e6b7ca0313d56f0cb56beef500764feaada3d16
JoozFerreira <54817143+joozferreira@users.noreply.github.com>
2023-11-13 00:27:55 +0000

    Add logic to start button and update of grid

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Global variable to define the settings of the game as an array
 const settings = ['classic', 16];
+const screen = document.getElementById("screen");
 
 // Change of game mode
 const buttons = Array.from(document.getElementsByClassName("mode"));
@@ -24,4 +25,36 @@ sliderSize.addEventListener("input", () => {
   let selectedGrid = document.getElementById("selected-grid");
   selectedGrid.textContent = `${sliderSize.value} X ${sliderSize.value}`;
   settings[1] = sliderSize.value;
+});
+
+// Define function to update the grid
+function createGrid(size) {
+  // Clear current grid
+  if (screen.lastElementChild !== null) {
+    screen.removeChild(screen.lastElementChild);
+  }
+
+  // Create new grid
+  const container = document.createElement("div");
+  container.setAttribute("id", "container");
+  screen.appendChild(container);
+
+  // Add squares to container
+  for (let i = 0; i < Math.pow(size, 2); i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.setAttribute("class", "square");
+    container.appendChild(gridSquare);
+  }
+
+  // Add width to squares and event listener
+  const squares = document.getElementsByClassName("square");
+  Array.from(squares).forEach( (square) => {
+    square.style.width = (450 / size) + "px";
+    square.addEventListener("hover", paintSquare(e));
+  });
+}
+
+const start = document.getElementById("start");
+start.addEventListener('click', () => {
+  createGrid(settings[1]);
 });
